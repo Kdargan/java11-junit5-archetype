c96d47183154403b838b0724cd81f79ceaa1b682
Kdargaan <136730198+Kdargan@users.noreply.github.com>
2024-08-23 01:58:03 +0530

    Update kdslave2_configfile.sh

--- kdslave2_configfile.sh
+++ kdslave2_configfile.sh
@@ -1,1 +1,18 @@
+#! /bin/bash
+
+sudo su -
+sudo yum install git -y
+sudo yum install maven -y
+sudo yum install java-11-openjdk.x86_64 -y
+
+if { -d java11-junit5-archetype}
+then
+echo "Git repo already clones. Already existed!!!"
+cd /root/java11-junit5-archetype
+git pull origin java11-junit5-archetype
+else
+git clone https://github.com/Kdargan/java11-junit5-archetype.git
+cd java11-junit5-archetype
+git checkout master
+fi
 
